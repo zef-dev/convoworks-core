58dd3fdc433df30acc13a7ee0fb0a753e159470d
Mark Marcec <mark@zef.dev>
2021-01-07 12:23:01 +0100

    add new test data sets and test in AmazonAdapterTest.php

--- tests/unit/Convo/Core/Adapters/Alexa/AmazonAdapterTest.php
+++ tests/unit/Convo/Core/Adapters/Alexa/AmazonAdapterTest.php
@@ -60,6 +60,78 @@ class AmazonAdapterTest extends ConvoTestCase
         $this->assertEquals(false, $amazonCommandRequest->getIsDisplayInterfaceEnabled());
     }
 
+    /**
+     * @dataProvider getRegularNextRequest
+     * @param $getRegularNextRequest
+     * @throws Exception
+     */
+    public function testRegularNextRequest($getRegularNextRequest)
+    {
+        $amazonCommandRequest = new AmazonCommandRequest($this->_logger, self::SERVICE_ID, $getRegularNextRequest);
+        $amazonCommandRequest->init();
+        $this->assertEquals(false, $amazonCommandRequest->isMediaRequest());
+    }
+
+    /**
+     * @dataProvider getNextMediaRequest
+     * @param $getNextMediaRequest
+     * @throws Exception
+     */
+    public function testNextMediaRequest($getNextMediaRequest)
+    {
+        $amazonCommandRequest = new AmazonCommandRequest($this->_logger, self::SERVICE_ID, $getNextMediaRequest);
+        $amazonCommandRequest->init();
+        $this->assertEquals(true, $amazonCommandRequest->isMediaRequest());
+    }
+
+    /**
+     * @dataProvider getRegularPreviousRequest
+     * @param $getRegularPreviousRequest
+     * @throws Exception
+     */
+    public function testRegularPreviousRequest($getRegularPreviousRequest)
+    {
+        $amazonCommandRequest = new AmazonCommandRequest($this->_logger, self::SERVICE_ID, $getRegularPreviousRequest);
+        $amazonCommandRequest->init();
+        $this->assertEquals(false, $amazonCommandRequest->isMediaRequest());
+    }
+
+    /**
+     * @dataProvider getPreviousMediaRequest
+     * @param $getPreviousMediaRequest
+     * @throws Exception
+     */
+    public function testPreviousMediaRequest($getPreviousMediaRequest)
+    {
+        $amazonCommandRequest = new AmazonCommandRequest($this->_logger, self::SERVICE_ID, $getPreviousMediaRequest);
+        $amazonCommandRequest->init();
+        $this->assertEquals(true, $amazonCommandRequest->isMediaRequest());
+    }
+
+    /**
+     * @dataProvider getContinuePlaybackRequest
+     * @param $getContinuePlaybackRequest
+     * @throws Exception
+     */
+    public function testContinuePlaybackRequest($getContinuePlaybackRequest)
+    {
+        $amazonCommandRequest = new AmazonCommandRequest($this->_logger, self::SERVICE_ID, $getContinuePlaybackRequest);
+        $amazonCommandRequest->init();
+        $this->assertEquals(true, $amazonCommandRequest->isMediaRequest());
+    }
+
+    /**
+     * @dataProvider getContinuePlaybackAfterPlayDirectiveRequest
+     * @param $getContinuePlaybackAfterPlayDirectiveRequest
+     * @throws Exception
+     */
+    public function testContinuePlaybackAfterPlayDirectiveRequest($getContinuePlaybackAfterPlayDirectiveRequest)
+    {
+        $amazonCommandRequest = new AmazonCommandRequest($this->_logger, self::SERVICE_ID, $getContinuePlaybackAfterPlayDirectiveRequest);
+        $amazonCommandRequest->init();
+        $this->assertEquals(true, $amazonCommandRequest->isMediaRequest());
+    }
+
     // data providers form real json requests
     public function getLaunchRequestWithViewport() {
         return $this->_establishTestData(__DIR__ . './data/launch_request_with_viewport.json');
@@ -72,4 +144,29 @@ class AmazonAdapterTest extends ConvoTestCase
     public function getLaunchRequestWithDisplayInterface() {
         return $this->_establishTestData(__DIR__ . './data/launch_request_with_display_interface.json');
     }
+
+    public function getRegularNextRequest() {
+        return $this->_establishTestData(__DIR__ . './data/next_intent_request.json');
+    }
+
+    public function getNextMediaRequest() {
+        return $this->_establishTestData(__DIR__ . './data/next_intent_request_after_play_directive.json');
+    }
+
+    public function getRegularPreviousRequest() {
+        return $this->_establishTestData(__DIR__ . './data/previous_intent_request.json');
+    }
+
+    public function getPreviousMediaRequest() {
+        return $this->_establishTestData(__DIR__ . './data/previous_intent_request_after_play_directive.json');
+    }
+
+    public function getContinuePlaybackRequest() {
+        return $this->_establishTestData(__DIR__ . './data/continue_playback_intent.json');
+    }
+
+    public function getContinuePlaybackAfterPlayDirectiveRequest() {
+        return $this->_establishTestData(__DIR__ . './data/continue_playback_intent_after_play_directive.json');
+    }
+
 }
