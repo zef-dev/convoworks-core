2631837ee024346a2821bee612ee98a1c1795483
Tihomir Dmitrovic <tole.car@gmail.com>
2021-02-23 23:53:27 +0100

    props descriptions and preview shorten and bold

--- src/Convo/Pckg/Visuals/VisualsPackageDefinition.php
+++ src/Convo/Pckg/Visuals/VisualsPackageDefinition.php
@@ -22,14 +22,14 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                 $this->getNamespace(),
                 '\Convo\Pckg\Visuals\Elements\ListTitleElement',
                 'List Title',
-                'Initiates list layout and sets title and type.',
+                'Initiates list layout and sets title and list type.',
                 array(
                     'list_title' => array(
                         'editor_type' => 'text',
                         'editor_properties' => array(),
                         'defaultValue' => '',
                         'name' => 'List title',
-                        'description' => 'Title of the content that is in the list.',
+                        'description' => 'Title line displayed in front of list.',
                         'valueType' => 'string'
                     ),
                     'list_template' => array(
@@ -46,8 +46,8 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                         'type' => 'html',
                         'template' => '<div class="code">' .
                         '<ul class="list-unstyled">' .
-                        ' <li>List name: {{component.properties.list_title}}</li>' .
-                        ' <li>List template: {{component.properties.list_template}}</li>' .
+                        ' <li>Title: <b>{{component.properties.list_title}}</b></li>' .
+                        ' <li>Template: <b>{{component.properties.list_template}}</b></li>' .
                         '</ul>' .
                         '</div>'
                     ),
@@ -68,7 +68,7 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                         'editor_type' => 'text',
                         'editor_properties' => array(),
                         'defaultValue' => '',
-                        'name' => 'List item key',
+                        'name' => 'Key',
                         'description' => 'Key of the item which is in the list.',
                         'valueType' => 'string'
                     ),
@@ -76,7 +76,7 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                         'editor_type' => 'text',
                         'editor_properties' => array(),
                         'defaultValue' => '',
-                        'name' => 'List item title',
+                        'name' => 'Title',
                         'description' => 'Title of the item which is in the list.',
                         'valueType' => 'string'
                     ),
@@ -84,7 +84,7 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                         'editor_type' => 'text',
                         'editor_properties' => array(),
                         'defaultValue' => '',
-                        'name' => 'List item description 1',
+                        'name' => 'Description 1',
                         'description' => 'Description of the item which is in the list. (works with Google Assistant and Alexa)',
                         'valueType' => 'string'
                     ),
@@ -92,7 +92,7 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                         'editor_type' => 'text',
                         'editor_properties' => array(),
                         'defaultValue' => '',
-                        'name' => 'List item description 2',
+                        'name' => 'Description 2',
                         'description' => 'Secondary description of the item which is in the list. (works with Alexa only)',
                         'valueType' => 'string'
                     ),
@@ -100,28 +100,28 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                         'editor_type' => 'text',
                         'editor_properties' => array(),
                         'defaultValue' => '',
-                        'name' => 'List item image url',
-                        'description' => 'Link to the image of an item in the list.',
+                        'name' => 'Image url',
+                        'description' => 'Url of the item image',
                         'valueType' => 'string'
                     ),
                     'list_item_image_text' => array(
                         'editor_type' => 'text',
                         'editor_properties' => array(),
                         'defaultValue' => '',
-                        'name' => 'List item image text',
-                        'description' => 'Accessibility text of the image of an item in the list. (Required if you want to display the image.)',
+                        'name' => 'Image text',
+                        'description' => 'Accessibility text for the item image. (Required if you want to display the image.)',
                         'valueType' => 'string'
                     ),
                     '_preview_angular' => array(
                         'type' => 'html',
                         'template' => '<div class="code">' .
                             '<ul class="list-unstyled">' .
-                            ' <li>List item key: {{component.properties.list_item_key}}</li>' .
-                            ' <li>List item title: {{component.properties.list_item_title}}</li>' .
-                            ' <li>List item description 1: {{component.properties.list_item_description_1}}</li>' .
-                            ' <li>List item description 2: {{component.properties.list_item_description_2}}</li>' .
-                            ' <li>List item image URL: {{component.properties.list_item_image_url}}</li>' .
-                            ' <li>List item image text: {{component.properties.list_item_image_text}}</li>' .
+                            ' <li>Key: <b>{{component.properties.list_item_key}}</b></li>' .
+                            ' <li>Title: <b>{{component.properties.list_item_title}}</b></li>' .
+                            ' <li>Description 1: <b>{{component.properties.list_item_description_1}}</b></li>' .
+                            ' <li>Description 2: <b>{{component.properties.list_item_description_2}}</b></li>' .
+                            ' <li>Image URL: <b>{{component.properties.list_item_image_url}}</b></li>' .
+                            ' <li>Image text: <b>{{component.properties.list_item_image_text}}</b></li>' .
                             '</ul>' .
                             '</div>'
                     ),
@@ -135,7 +135,7 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
             new \Convo\Core\Factory\ComponentDefinition(
                 $this->getNamespace(),
                 '\Convo\Pckg\Visuals\Elements\ListElement',
-                'List',
+                'x!List',
                 '*** DEPRECATED *** Iterates over a collection and renders a visual representation for each item in the list. (Works with devices that have the screen output capability.)',
                 array(
                     'list_title' => array(
@@ -262,7 +262,7 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                         'editor_type' => 'text',
                         'editor_properties' => array(),
                         'defaultValue' => '',
-                        'name' => 'Data item title',
+                        'name' => 'Title',
                         'description' => 'Title of the item which will be displayed on the card.',
                         'valueType' => 'string'
                     ),
@@ -270,7 +270,7 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                         'editor_type' => 'text',
                         'editor_properties' => array(),
                         'defaultValue' => '',
-                        'name' => 'Data item subtitle',
+                        'name' => 'Subtitle',
                         'description' => 'Subtitle of the item which will be displayed on the card.',
                         'valueType' => 'string'
                     ),
@@ -278,7 +278,7 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                         'editor_type' => 'desc',
                         'editor_properties' => array(),
                         'defaultValue' => '',
-                        'name' => 'Data item description 1',
+                        'name' => 'Description 1',
                         'description' => 'Primary description of the item which will be displayed on the card.',
                         'valueType' => 'string'
                     ),
@@ -286,7 +286,7 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                         'editor_type' => 'desc',
                         'editor_properties' => array(),
                         'defaultValue' => '',
-                        'name' => 'Data item description 2',
+                        'name' => 'Description 2',
                         'description' => 'Secondary description of the item which will be displayed on the card. (works with Alexa only)',
                         'valueType' => 'string'
                     ),
@@ -294,7 +294,7 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                         'editor_type' => 'desc',
                         'editor_properties' => array(),
                         'defaultValue' => '',
-                        'name' => 'Data item description 3',
+                        'name' => 'Description 3',
                         'description' => 'Tertiary description of the item which will be displayed on the card. (works with Alexa only)',
                         'valueType' => 'string'
                     ),
@@ -302,7 +302,7 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                         'editor_type' => 'text',
                         'editor_properties' => array(),
                         'defaultValue' => '',
-                        'name' => 'Data item image url',
+                        'name' => 'Image url',
                         'description' => 'Link to the image of an item in the card.',
                         'valueType' => 'string'
                     ),
@@ -310,7 +310,7 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                         'editor_type' => 'text',
                         'editor_properties' => array(),
                         'defaultValue' => '',
-                        'name' => 'Data item image text',
+                        'name' => 'Image text',
                         'description' => 'Accessibility text of the image of an item in the card. (Required if you want to display the image.)',
                         'valueType' => 'string'
                     ),
@@ -318,14 +318,14 @@ class VisualsPackageDefinition extends AbstractPackageDefinition
                         'type' => 'html',
                         'template' => '<div class="code">' .
                             '<ul class="list-unstyled">' .
-                            ' <li>Back button: {{component.properties.back_button}}</li>' .
-                            ' <li>Data item title: {{component.properties.data_item_title}}</li>' .
-                            ' <li>Data item subtitle: {{component.properties.data_item_subtitle}}</li>' .
-                            ' <li>Data item description 1: {{component.properties.data_item_description_1}}</li>' .
-                            ' <li>Data item description 2: {{component.properties.data_item_description_2}}</li>' .
-                            ' <li>Data item description 3: {{component.properties.data_item_description_3}}</li>' .
-                            ' <li>Data item image URL: {{component.properties.data_item_image_url}}</li>' .
-                            ' <li>Data item image text: {{component.properties.data_item_image_text}}</li>' .
+                            ' <li>Back button: <b>{{component.properties.back_button}}</b></li>' .
+                            ' <li>Title: <b>{{component.properties.data_item_title}}</b></li>' .
+                            ' <li>Subtitle: <b>{{component.properties.data_item_subtitle}}</b></li>' .
+                            ' <li>Description 1: <b>{{component.properties.data_item_description_1}}</b></li>' .
+                            ' <li>Description 2: <b>{{component.properties.data_item_description_2}}</b></li>' .
+                            ' <li>Description 3: <b>{{component.properties.data_item_description_3}}</b></li>' .
+                            ' <li>Image URL: <b>{{component.properties.data_item_image_url}}</b></li>' .
+                            ' <li>Image text: <b>{{component.properties.data_item_image_text}}</b></li>' .
                             '</ul>' .
                             '</div>'
                     ),
