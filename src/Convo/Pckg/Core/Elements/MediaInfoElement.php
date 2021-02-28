b838441c1908186297f52739b32eecbefc7c8265
Tihomir Dmitrovic <tole.car@gmail.com>
2021-02-28 17:58:02 +0100

    override evaluateString

--- src/Convo/Pckg/Core/Elements/MediaInfoElement.php
+++ src/Convo/Pckg/Core/Elements/MediaInfoElement.php
@@ -81,6 +81,15 @@ class MediaInfoElement extends AbstractWorkflowContainerComponent implements ICo
         return $this->getService()->findContext($contextId)->getComponent();
     }
     
+    /**
+     * @inheritDoc
+     */
+    public function evaluateString($string, $context = [])
+    {
+        $own_params		=	$this->getService()->getAllComponentParams( $this);
+        return parent::evaluateString( $string, array_merge( $own_params, $context));
+    }
+    
     // UTIL
     public function __toString() {
         return parent::__toString().'['.$this->_contextId.']['.$this->_mediaInfoVar.']';
