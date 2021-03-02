ea673ab25ff768f78e444a459fc7dab690d7faad
Tihomir Dmitrovic <tole.car@gmail.com>
2021-03-02 10:22:44 +0100

    plain and id3 support mp3 file

--- src/Convo/Core/Media/Mp3File.php
+++ src/Convo/Core/Media/Mp3File.php
@@ -3,56 +3,24 @@
 
 namespace Convo\Core\Media;
 
-use wapmorgan\Mp3Info\Mp3Info;
-
 class Mp3File implements IAudioFile
 {
-    private $_filePath;
-    private $_fileMetaData;
-    
     private $_fileUrl;
-    private $_artworkUrl;
-    private $_backgroundUrl;
-
     private $_song;
     private $_artist;
     
-    public function __construct( $filePath, $fileUrl, $artwork=null, $background=null, $song=null, $artist=null)
+    private $_artworkUrl;
+    private $_backgroundUrl;
+    
+    public function __construct( $fileUrl, $song, $artist, $artwork=null, $background=null)
     {
-        $this->_filePath        =   $filePath;
-        
         $this->_fileUrl         =   $fileUrl;
+        $this->_song            =   $song;
+        $this->_artist          =   $artist;
+        
         $this->_artworkUrl      =   $artwork;
         $this->_backgroundUrl   =   $background;
 
-        $this->_song            =   $song;
-        $this->_artist          =   $artist;
-    }
-    
-    public function getMetadata()
-    {
-        if ( !isset( $this->_fileMetaData) && !empty( $this->getFilePath()))
-        {
-            try {
-                $info                   =   new Mp3Info( $this->getFilePath(), true);
-                $this->_fileMetaData    =   $info->tags;
-            } catch ( \Exception $e) {
-                $this->_fileMetaData    =   [];
-            }
-            
-            if ( !isset( $this->_fileMetaData['artist']) || empty( $this->_fileMetaData['artist'])) {
-                $this->_fileMetaData['artist'] = 'N/A';
-            }
-    
-            if ( !isset( $this->_fileMetaData['song']) || empty( $this->_fileMetaData['song'])) {
-                $name   =   basename( $this->getFilePath(), '.mp3');
-                $name   =   str_replace( "_", " ", $name);
-                $name   =   str_replace( "-", " ", $name);
-                $this->_fileMetaData['song']    =   $name;
-            }
-        }
-        
-        return $this->_fileMetaData;
     }
 
     public function getFileUrl() {
@@ -60,17 +28,11 @@ class Mp3File implements IAudioFile
     }
     
     public function getSongTitle() {
-        if ( $this->_song) {
-            return $this->_song;
-        }
-        return $this->getMetadata()['song'];
+        return $this->_song;
     }
     
     public function getArtist() {
-        if ( $this->_artist) {
-            return $this->_artist;
-        }
-        return $this->getMetadata()['artist'];
+        return $this->_artist;
     }
 
     public function getSongImageUrl() {
@@ -83,7 +45,7 @@ class Mp3File implements IAudioFile
 
     public function __toString()
     {
-        return get_class( $this).'['.$this->_filePath.']['.$this->_fileUrl.']';
+        return get_class( $this).'['.$this->_fileUrl.']['.$this->_song.']['.$this->_artist.']['.$this->_artworkUrl.']['.$this->_backgroundUrl.']';
     }
 }
 
---
+++ src/Convo/Core/Media/Mp3Id3File.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace Convo\Core\Media;
+
+use wapmorgan\Mp3Info\Mp3Info;
+
+class Mp3Id3File extends Mp3File
+{
+    private $_filePath;
+    private $_fileMetaData;
+    
+    public function __construct( $filePath, $fileUrl, $artwork=null, $background=null, $song=null, $artist=null)
+    {
+        parent::__construct( $fileUrl, $song, $artist, $artwork, $background);
+        
+        $this->_filePath        =   $filePath;
+    }
+    
+    public function getMetadata()
+    {
+        if ( !isset( $this->_fileMetaData) && !empty( $this->_filePath))
+        {
+            try {
+                $info                   =   new Mp3Info( $this->_filePath, true);
+                $this->_fileMetaData    =   $info->tags;
+            } catch ( \Exception $e) {
+                $this->_fileMetaData    =   [];
+            }
+            
+            if ( !isset( $this->_fileMetaData['artist']) || empty( $this->_fileMetaData['artist'])) {
+                $this->_fileMetaData['artist'] = '';
+            }
+    
+            if ( !isset( $this->_fileMetaData['song']) || empty( $this->_fileMetaData['song'])) {
+                $name   =   basename( $this->_filePath, '.mp3');
+                $name   =   str_replace( "_", " ", $name);
+                $name   =   str_replace( "-", " ", $name);
+                $this->_fileMetaData['song']    =   $name;
+            }
+        }
+        
+        return $this->_fileMetaData;
+    }
+    
+    public function getSongTitle() {
+        return parent::getSongTitle() ? parent::getSongTitle() : $this->getMetadata()['song'];
+    }
+    
+    public function getArtist() {
+        return parent::getArtist() ? parent::getArtist() : $this->getMetadata()['artist'];
+    }
+
+    public function __toString()
+    {
+        return parent::__toString().'['.$this->_filePath.']';
+    }
+}
+
