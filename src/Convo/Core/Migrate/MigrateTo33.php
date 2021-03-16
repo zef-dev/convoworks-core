c57cb9a5cad71cb422015add7fa020f8d5f4039d
mmarcec007 <mark.marcec@gmail.com>
2021-03-16 11:56:01 +0100

    update MigrateTo33.php to include populate account linking configuration

--- src/Convo/Core/Migrate/MigrateTo33.php
+++ src/Convo/Core/Migrate/MigrateTo33.php
@@ -19,7 +19,17 @@ class MigrateTo33 extends AbstractMigration
     {
         if (isset($config["amazon"])) {
             if (!isset($config["amazon"]["account_linking_mode"])) {
-                $config["amazon"]["account_linking_mode"] = 'installation';
+                $config["amazon"]["account_linking_mode"] = 'amazon';
+            }
+
+            if (!isset($config["amazon"]["account_linking_config"])) {
+                $config["amazon"]["account_linking_config"]['skip_on_enablement'] = true;
+                $config["amazon"]["account_linking_config"]['authorization_url'] = 'https://www.amazon.com/ap/oa';
+                $config["amazon"]["account_linking_config"]['access_token_url'] = 'https://api.amazon.com/auth/o2/token';
+                $config["amazon"]["account_linking_config"]['client_id'] = '';
+                $config["amazon"]["account_linking_config"]['client_secret'] = '';
+                $config["amazon"]["account_linking_config"]['scopes'] = '';
+                $config["amazon"]["account_linking_config"]['domains'] = '';
             }
         }
         return parent::migrateConfig($config);
