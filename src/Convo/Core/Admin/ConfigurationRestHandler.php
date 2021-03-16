e9f0dd5e70754e316655fd0a1053e439eeb15e1b
mmarcec007 <mark.marcec@gmail.com>
2021-03-16 08:20:16 +0100

    remove CONVO_AMAZON_ACCOUNT_LINKING_OFFERS in ConfigurationRestHandler class

--- src/Convo/Core/Admin/ConfigurationRestHandler.php
+++ src/Convo/Core/Admin/ConfigurationRestHandler.php
@@ -65,11 +65,6 @@ class ConfigurationRestHandler implements \Psr\Http\Server\RequestHandlerInterfa
                 ['value' => 'MEDIUM', 'name' => 'Medium'],
                 ['value' => 'HIGH', 'name' => 'High']
             ],
-            'CONVO_AMAZON_ACCOUNT_LINKING_OFFERS' => [
-                ['value' => 'installation', 'name' => 'Installation'],
-                ['value' => 'amazon', 'name' => 'Amazon'],
-                ['value' => 'something_else', 'name' => 'Something Else']
-            ],
             'CONVO_AMAZON_SKILL_ENDPOINT_SSL_CERTIFICATE' => [
                 ['value' => AmazonSkillManifest::CERTIFICATE_TYPE_TRUSTED, 'description' => 'My Development endpoint has a certificate from a trusted certificate authority'],
                 ['value' => AmazonSkillManifest::CERTIFICATE_TYPE_WILDCARD, 'description' => 'My Development endpoint is a sub-domain of a domain that has a wildcard certificate from a certificate authority'],
