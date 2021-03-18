ae2eb38338301ac10be4625a6f3ed76583df3155
mmarcec007 <mark.marcec@gmail.com>
2021-03-18 09:54:36 +0100

    add IHttpFactory and IServiceDataProvider in AmazonPackageDefinition class

--- src/Convo/Pckg/Alexa/AmazonPackageDefinition.php
+++ src/Convo/Pckg/Alexa/AmazonPackageDefinition.php
@@ -8,8 +8,24 @@ class AmazonPackageDefinition extends AbstractPackageDefinition
 {
 	const NAMESPACE	=	'convo-alexa';
 
-	public function __construct( \Psr\Log\LoggerInterface $logger)
-	{
+    /**
+     * @var \Convo\Core\Util\IHttpFactory
+     */
+    private $_httpFactory;
+
+    /**
+     * @var \Convo\Core\IServiceDataProvider
+     */
+    private $_convoServiceDataProvider;
+
+	public function __construct(
+	    \Psr\Log\LoggerInterface $logger,
+        \Convo\Core\Util\IHttpFactory $httpFactory,
+        \Convo\Core\IServiceDataProvider $convoServiceDataProvider
+    ) {
+        $this->_httpFactory = $httpFactory;
+        $this->_convoServiceDataProvider = $convoServiceDataProvider;
+
 		parent::__construct( $logger, self::NAMESPACE, __DIR__);
 	}
 
