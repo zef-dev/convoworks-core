737ca4af16f68e53caaf117ade2ceb253578e611
mmarcec007 <mark.marcec@gmail.com>
2021-04-13 09:39:30 +0200

    implement getStatus in FacebookMessengerServicePublisher class

--- src/Convo/Core/Adapters/Fbm/FacebookMessengerServicePublisher.php
+++ src/Convo/Core/Adapters/Fbm/FacebookMessengerServicePublisher.php
@@ -125,4 +125,14 @@ class FacebookMessengerServicePublisher extends \Convo\Core\Publish\AbstractServ
     {
         // TODO: Implement delete() method.
     }
+
+    public function getStatus()
+    {
+        $config = $this->_convoServiceDataProvider->getServicePlatformConfig($this->_user, $this->_serviceId, IPlatformPublisher::MAPPING_TYPE_DEVELOP);
+        $status = ['status' => IPlatformPublisher::SERVICE_PROPAGATION_STATUS_FINISHED];
+        if (isset($config[$this->getPlatformId()]['webhook_build_status'])) {
+            $status['status'] = $config[$this->getPlatformId()]['webhook_build_status'];
+        }
+        return $status;
+    }
 }
