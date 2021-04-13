73b808490e39e0d458b87d41963abcd16da46434
mmarcec007 <mark.marcec@gmail.com>
2021-04-13 08:44:22 +0200

    implement _performServicePlatformPropagateStatusPathServiceIdPathPlatformIdGet in ServicePlatformConfigRestHandler class to get status of an platform

--- src/Convo/Core/Admin/ServicePlatformConfigRestHandler.php
+++ src/Convo/Core/Admin/ServicePlatformConfigRestHandler.php
@@ -92,6 +92,12 @@ class ServicePlatformConfigRestHandler implements RequestHandlerInterface
 		        $request, $user, $route->get( 'serviceId'), $route->get( 'platformId'));
 		}
 
+		if ( $info->get() && $route = $info->route( 'service-platform-status/{serviceId}/{platformId}'))
+		{
+		    return $this->_performServicePlatformPropagateStatusPathServiceIdPathPlatformIdGet(
+		        $request, $user, $route->get( 'serviceId'), $route->get( 'platformId'));
+		}
+
 		throw new \Convo\Core\Rest\NotFoundException( 'Could not map ['.$info.']');
 	}
 
@@ -199,6 +205,15 @@ class ServicePlatformConfigRestHandler implements RequestHandlerInterface
 
 	    return $this->_httpFactory->buildResponse( $data);
 	}
+
+    private function _performServicePlatformPropagateStatusPathServiceIdPathPlatformIdGet(
+        \Psr\Http\Message\ServerRequestInterface $request, \Convo\Core\IAdminUser $user, $serviceId, $platformId)
+    {
+        $publisher = $this->_platformPublisherFactory->getPublisher( $user, $serviceId, $platformId);
+        $status = $publisher->getStatus();
+
+        return $this->_httpFactory->buildResponse( $status);
+    }
 
 
 	private function _loadServicePlatformConfig(\Psr\Http\Message\ServerRequestInterface $request, \Convo\Core\IAdminUser $user, $serviceId)
