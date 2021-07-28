b5a756f8acf5cd278ad37e5e67721d16fd9c5c58
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2021-07-28 15:04:41 +0200

    Text response element overhaul

--- src/Convo/Pckg/Core/Elements/TextResponseElement.php
+++ src/Convo/Pckg/Core/Elements/TextResponseElement.php
@@ -39,15 +39,16 @@ class TextResponseElement extends \Convo\Core\Workflow\AbstractWorkflowComponent
 
 	public function read( \Convo\Core\Workflow\IConvoRequest $request, \Convo\Core\Workflow\IConvoResponse $response)
 	{
-		$this->_logger->info( 'Raw text ['.$this->_text.']');
+		$this->_logger->info('Raw text ['.$this->_text.']');
 
-		$text	=	$this->evaluateString( $this->_text);
+		$text = $this->evaluateString($this->_text);
 
-		if ( $this->_break) {
-			$text	.=	'<break time="'.$this->_break.'"/>';
+		if ($this->_break) {
+			$text .= '<break time="'.$this->_break.'"/>';
 		}
 
-		$this->_addPlatformText($response, $text, $this->_type);
+        $type = $this->evaluateString($this->_type);
+		$this->_addPlatformText($response, $text, $type);
 	}
 
 	public function getSpeech()
@@ -67,46 +68,58 @@ class TextResponseElement extends \Convo\Core\Workflow\AbstractWorkflowComponent
 	// UTIL
 	public function __toString()
 	{
-		return parent::__toString().'['.$this->_type.']['.$this->_break.']['.$this->_text.']';
+		return parent::__toString().'['.$this->evaluateString($this->_type).']['.$this->_break.']['.$this->_text.']';
 	}
 
     private function _addPlatformText($response, $text, $type): void
     {
+        $append = $this->evaluateString($this->_append);
+
         if ($type === self::TYPE_DEFAULT)
         {
             $this->_logger->info( 'Adding text ['.$text.']');
             if (is_a($response, 'Convo\Core\Adapters\Alexa\AmazonCommandResponse'))
             {
-                /* @var AmazonCommandResponse $response */
-                if ($this->_alexaDomain !== self::ALEXA_DOMAIN) {
-                    $response->addDomainText($this->_alexaDomain, $text, $this->_append);
-                } else if ($this->_alexaEmotion !== self::ALEXA_EMOTION_TYPE) {
-                    $response->addEmotionText($this->_alexaEmotion, $this->_alexaEmotionIntensity, $text, $this->_append);
-                } else if ($this->_alexaEmotion === self::ALEXA_EMOTION_TYPE && $this->_alexaDomain === self::ALEXA_DOMAIN) {
-                    $response->addText($text, $this->_append);
+                $domain = $this->evaluateString($this->_alexaDomain);
+                $emotion = $this->evaluateString($this->_alexaEmotion);
+                $emotion_intensity = $this->evaluateString($this->_alexaEmotionIntensity);
+
+                /** @var AmazonCommandResponse $response */
+                if ($domain !== self::ALEXA_DOMAIN) {
+                    $response->addDomainText($domain, $text, $append);
+                } else if ($emotion !== self::ALEXA_EMOTION_TYPE) {
+                    $response->addEmotionText($emotion, $emotion_intensity, $text, $append);
+                } else if ($emotion === self::ALEXA_EMOTION_TYPE && $domain === self::ALEXA_DOMAIN) {
+                    $response->addText($text, $append);
                 }
             } else {
                 /* @var DefaultTextCommandResponse $response */
-                $response->addText($text, $this->_append);
+                $response->addText($text, $append);
             }
         }
         else if ($type === self::TYPE_REPROMPT)
         {
             $this->_logger->info( 'Adding reprompt text ['.$text.']');
-            if (is_a($response, 'Convo\Core\Adapters\Alexa\AmazonCommandResponse')) {
-                /* @var AmazonCommandResponse $response */
-                if ($this->_alexaEmotion !== self::ALEXA_EMOTION_TYPE) {
-                    $response->addEmotionRepromptText($this->_alexaEmotion, $this->_alexaEmotionIntensity, $text, $this->_append);
+            
+            if (is_a($response, 'Convo\Core\Adapters\Alexa\AmazonCommandResponse'))
+            {
+                $domain = $this->evaluateString($this->_alexaDomain);
+                $emotion = $this->evaluateString($this->_alexaEmotion);
+                $emotion_intensity = $this->evaluateString($this->_alexaEmotionIntensity);
+
+                /** @var AmazonCommandResponse $response */
+                if ($emotion !== self::ALEXA_EMOTION_TYPE) {
+                    $response->addEmotionRepromptText($emotion, $emotion_intensity, $text, $append);
                 }
-                if ($this->_alexaDomain !== self::ALEXA_DOMAIN) {
-                    $response->addDomainRepromptText($this->_alexaDomain, $text, $this->_append);
+                if ($domain !== self::ALEXA_DOMAIN) {
+                    $response->addDomainRepromptText($domain, $text, $append);
                 }
-                if ($this->_alexaEmotion === self::ALEXA_EMOTION_TYPE && $this->_alexaDomain === self::ALEXA_DOMAIN) {
-                    $response->addRepromptText($text, $this->_append);
+                if ($emotion === self::ALEXA_EMOTION_TYPE && $domain === self::ALEXA_DOMAIN) {
+                    $response->addRepromptText($text, $append);
                 }
             } else {
-                /* @var DefaultTextCommandResponse $response */
-                $response->addRepromptText($text, $this->_append);
+                /** @var DefaultTextCommandResponse $response */
+                $response->addRepromptText($text, $append);
             }
         } else {
             throw new \Exception( 'Unexpected type ['.$type.']');
