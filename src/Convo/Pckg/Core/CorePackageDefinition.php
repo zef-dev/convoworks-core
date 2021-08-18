558cc6ad9c8a15da2c704060d21ed9a6444b81b6
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2021-08-18 17:00:11 +0200

    Use core function provider

--- src/Convo/Pckg/Core/CorePackageDefinition.php
+++ src/Convo/Pckg/Core/CorePackageDefinition.php
@@ -8,6 +8,7 @@ use Convo\Core\Intent\EntityModel;
 use Symfony\Component\ExpressionLanguage\ExpressionFunction;
 use Psr\SimpleCache\CacheInterface;
 use Convo\Core\Workflow\IRunnableBlock;
+use Convo\Pckg\Core\CoreFunctionProvider;
 
 class CorePackageDefinition extends AbstractPackageDefinition
 {
@@ -128,143 +129,8 @@ class CorePackageDefinition extends AbstractPackageDefinition
 
     public function getFunctions()
     {
-        $functions = [];
-
-        $functions[] = ExpressionFunction::fromPhp('count');
-        $functions[] = ExpressionFunction::fromPhp('rand');
-        $functions[] = ExpressionFunction::fromPhp('strtolower');
-        $functions[] = ExpressionFunction::fromPhp('strtoupper');
-        $functions[] = ExpressionFunction::fromPhp('date');
-        $functions[] = ExpressionFunction::fromPhp('is_numeric');
-        $functions[] = ExpressionFunction::fromPhp('is_int');
-        $functions[] = ExpressionFunction::fromPhp('intval');
-        $functions[] = ExpressionFunction::fromPhp('ceil');
-        $functions[] = ExpressionFunction::fromPhp('floor');
-        $functions[] = ExpressionFunction::fromPhp('strlen');
-        $functions[] = ExpressionFunction::fromPhp('array_rand');
-        $functions[] = ExpressionFunction::fromPhp('array_values');
-        $functions[] = ExpressionFunction::fromPhp('urlencode');
-        $functions[] = ExpressionFunction::fromPhp('str_replace');
-        $functions[] = ExpressionFunction::fromPhp('array_splice');
-        $functions[] = ExpressionFunction::fromPhp('is_array');
-        $functions[] = ExpressionFunction::fromPhp('in_array');
-        $functions[] = ExpressionFunction::fromPhp('array_search');
-        $functions[] = ExpressionFunction::fromPhp('array_merge');
-        $functions[] = ExpressionFunction::fromPhp('is_numeric');
-        $functions[] = ExpressionFunction::fromPhp('substr');
-        $functions[] = ExpressionFunction::fromPhp('stripos');
-        $functions[] = ExpressionFunction::fromPhp('strtotime');
-        $functions[] = ExpressionFunction::fromPhp('time');
-
-        $functions[] = new ExpressionFunction(
-            'array_shuffle',
-            function ($array) {
-                return sprintf('(is_array(%1$a) ? array_shuffle(%1$a) : %1$a', $array);
-            },
-            function($args, $array) {
-                if (!is_array($array)) {
-                    return $array;
-                }
-
-                $copy = $array;
-                shuffle($copy);
-                return $copy;
-            }
-        );
-
-        $functions[] = new ExpressionFunction(
-            'array_push',
-            function ($array, $item) {
-                return sprintf('(is_array(%1$a) ? array_push(%1$a, %2$i) : %1$a', $array, $item);
-            },
-            function($args, $array, $item) {
-                if (!is_array($array)) {
-                    return $array;
-                }
-
-                $copy = $array;
-                $copy[] = $item;
-                return $copy;
-            }
-        );
-
-        $functions[] = new ExpressionFunction(
-            'decode_special_chars',
-            function ($string) {
-                return sprintf('(is_string(%1$a) ? decode_special_chars(%1$a) : %1$a', $string);
-            },
-            function($args, $string) {
-                if (!is_string($string)) {
-                    return $string;
-                }
-
-                $string = html_entity_decode( $string, ENT_QUOTES);
-                $string = htmlspecialchars_decode( $string);
-                $string = urldecode( $string);
-                return $string;
-            }
-        );
-
-        $functions[] = new ExpressionFunction(
-            'array_sort_by_field',
-            function ($array, $field) {
-                return sprintf('(is_array(%1$a) ? array_sort_by_field(%1$a, %2$a) : %1$a', $array);
-            },
-            function($args, $array, $field) {
-                if (!is_array($array)) {
-                    return $array;
-                }
-//                 $this->_logger->debug( 'helou'.print_r( $args, true));
-                usort($array, function ($a, $b) use ($field) {return strcmp( $a[$field], $b[$field]);});
-                return $array;
-            }
-        );
-
-        $functions[] = new ExpressionFunction(
-            'ordinal',
-            function ($string) {
-                return sprintf('(is_numeric(%1$a) ? ordinal(%1$a) : %1$a', $string);
-            },
-            function($args, $string) {
-                if (!is_numeric($string)) {
-                    return $string;
-                }
-
-                $integer = intval( $string);
-                $ends = array('th','st','nd','rd','th','th','th','th','th','th');
-
-                if( (($integer % 100) >= 11) && (($integer%100) <= 13)) {
-                    return $integer. 'th';
-                } else {
-                    return $integer. $ends[$integer % 10];
-                }
-            }
-        );
-
-        $functions[] = new ExpressionFunction(
-            'human_concat',
-            function ($array,  $conjunction) {
-                return sprintf('(is_array(%1$a) ? human_concat(%1$a, %2$a) : %1$a', $array);
-            },
-
-            function($args, $array, $conjunction = null) {
-                if (!is_array($array)) {
-                    return $array;
-                }
-
-                $last  = array_slice( $array, -1);
-                $first = join(', ', array_slice($array, 0, -1));
-                $both  = array_filter( array_merge( array( $first), $last), 'strlen');
-
-                if( $conjunction) {
-                    return join(' '.$conjunction.' ', $both);
-                }
-
-                return join(', ', $both);
-            }
-        );
-
-        return $functions;
+        $provider = new CoreFunctionProvider();
+        return $provider->getFunctions();
     }
 
     /**
