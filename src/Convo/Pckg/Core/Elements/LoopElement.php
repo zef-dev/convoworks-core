8f679f51bb0639291c27d260a6b4246331438fd1
mmarcec007 <mark.marcec@gmail.com>
2021-12-13 10:55:10 +0100

    add the ability to iterate trough iterator too in LoopElement

--- src/Convo/Pckg/Core/Elements/LoopElement.php
+++ src/Convo/Pckg/Core/Elements/LoopElement.php
@@ -37,55 +37,67 @@ class LoopElement extends \Convo\Core\Workflow\AbstractWorkflowContainerComponen
 	public function read(\Convo\Core\Workflow\IConvoRequest $request, \Convo\Core\Workflow\IConvoResponse $response)
 	{
         $items = $this->evaluateString($this->_dataCollection);
-        
-        if ( !is_array( $items)) {
-            throw new \Exception( 'Excepted to find array for ['.$this->_dataCollection.'] got ['.gettype( $items).']');
-        }
-        
-        $slot_name = $this->evaluateString($this->_item);
 
-        $scope_type	= \Convo\Core\Params\IServiceParamsScope::SCOPE_TYPE_REQUEST;
-		$params = $this->getService()->getComponentParams( $scope_type, $this);
-		
-		$start = 0;
-		$end = count($items);
+        if (is_array($items) || $items instanceof \Iterator) {
+			$slot_name = $this->evaluateString($this->_item);
 
-		$offset = $this->evaluateString($this->_offset);
-		$limit = $this->evaluateString($this->_limit);
+			$scope_type	= \Convo\Core\Params\IServiceParamsScope::SCOPE_TYPE_REQUEST;
+			$params = $this->getService()->getComponentParams( $scope_type, $this);
 
-		if ($offset !== null) {
-			if ($offset > $end || $offset < 0) {
-				$this->_logger->warning('Offset ['.$offset.'] falls outside the range ['.$start.', '.$end.']. Starting from 0.');
+			$start = 0;
+			if ($items instanceof \Iterator) {
+				$end = iterator_count($items);
 			} else {
-				$start = $offset;
+				$end = count($items);
 			}
-		}
 
-		if ($limit !== null) {
-			/** @var int $limit */
-			$limit = abs($limit);
-			$end = min(($start + $limit), count($items));
-		}
+			$offset = $this->evaluateString($this->_offset);
+			$limit = $this->evaluateString($this->_limit);
 
-		for ($i = $start; $i < $end; ++$i) {
-			$val = $items[$i];
-
-			$params->setServiceParam($slot_name, [
-				'value' => $val,
-				'index' => $i,
-				'natural' => $i + 1,
-				'first' => $i === $start,
-				'last' => $i === ($end - 1)
-			]);
-
-			$loop_until = $this->evaluateString($this->_loop_until);
-			if (is_bool($loop_until) && $loop_until) {
-				$this->_logger->info('Exiting loop.');
-				break;
+			if ($offset !== null) {
+				if ($offset > $end || $offset < 0) {
+					$this->_logger->warning('Offset ['.$offset.'] falls outside the range ['.$start.', '.$end.']. Starting from 0.');
+				} else {
+					$start = $offset;
+				}
 			}
-			foreach ($this->_elements as $element) {
-				$element->read($request, $response);
+
+			if ($limit !== null) {
+				/** @var int $limit */
+				$limit = abs($limit);
+				$end = min(($start + $limit), $end);
 			}
+
+			$i = 0;
+			foreach ($items as $item) {
+				if ($i < $start) {
+					continue;
+				}
+
+				if ($i >= $end) {
+					break;
+				}
+
+				$params->setServiceParam($slot_name, [
+					'value' => $item,
+					'index' => $i,
+					'natural' => $i + 1,
+					'first' => $i === $start,
+					'last' => $i === ($end - 1)
+				]);
+
+				$loop_until = $this->evaluateString($this->_loop_until);
+				if (is_bool($loop_until) && $loop_until) {
+					$this->_logger->info('Exiting loop.');
+					break;
+				}
+				foreach ($this->_elements as $element) {
+					$element->read($request, $response);
+				}
+				$i++;
+			}
+        } else {
+			throw new \Exception( 'Excepted to find array for ['.$this->_dataCollection.'] got ['.gettype( $items).']');
 		}
 	}
 	
