8d1eb2fbea05e5fa53d9ed90f0a5fb4c9280934f
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2022-02-11 10:22:36 +0100

    Expose contexts as map in evaluation

--- src/Convo/Core/ConvoServiceInstance.php
+++ src/Convo/Core/ConvoServiceInstance.php
@@ -576,7 +576,12 @@ class ConvoServiceInstance implements \Convo\Core\Workflow\IWorkflowContainerCom
         $context			=	array_merge( $variables, $context);
 
         // CONTEXTS
-        $context			=	array_merge( [ 'contexts' => $this->_contexts], $context);
+        $context_map        =   [];
+        foreach ($this->_contexts as $context) {
+            $context_map[$context->getId()] = $context;
+        }
+
+        $context			=	array_merge(['contexts' => $context_map], $context);
 
         // REQUEST
         $objResolver		=	new ObjectResolver( $this->_request);
