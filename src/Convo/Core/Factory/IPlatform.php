0c6605ebd0c1c0867f41348ea14687b90ecc1c98
Tihomir Dmitrovic <tole.car@gmail.com>
2022-10-06 00:41:40 +0200

    Add platform publisher to platform interface

--- src/Convo/Core/Factory/IPlatform.php
+++ src/Convo/Core/Factory/IPlatform.php
@@ -3,6 +3,8 @@
 namespace Convo\Core\Factory;
 
 use Psr\Http\Server\RequestHandlerInterface;
+use Convo\Core\Publish\IPlatformPublisher;
+use Convo\Core\IAdminUser;
 
 interface IPlatform
 {
@@ -16,5 +18,13 @@ interface IPlatform
 	 * @return RequestHandlerInterface
 	 */
     public function getPublicRestHandler();
+ 
+
+    /**
+     * @param IAdminUser $user
+     * @param string $serviceId
+	 * @return IPlatformPublisher
+	 */
+    public function getPlatformPublisher( IAdminUser $user, $serviceId);
 
 }
