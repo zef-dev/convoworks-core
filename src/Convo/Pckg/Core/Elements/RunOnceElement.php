dcabb6449d243fcaeb43422ff9dcfb17e6364225
Tihomir Dmitrovic <tole.car@gmail.com>
2024-12-03 20:33:15 +0100

    Correct field ref

--- src/Convo/Pckg/Core/Elements/RunOnceElement.php
+++ src/Convo/Pckg/Core/Elements/RunOnceElement.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Convo\Pckg\Core\Elements;
 
@@ -10,14 +12,14 @@ use Convo\Core\Workflow\IOptionalElement;
 class RunOnceElement extends AbstractWorkflowContainerComponent implements IConversationElement, IOptionalElement
 {
     const PARAM_NAME_TRIGGERED  =   'triggered';
-    
+
     private $_scopeType;
 
     /**
      * @var \Convo\Core\Workflow\IConversationElement[]
      */
     private $_children;
-    
+
     /**
      * @var \Convo\Core\Workflow\IConversationElement[]
      */
@@ -25,18 +27,18 @@ class RunOnceElement extends AbstractWorkflowContainerComponent implements IConv
 
     public function __construct($properties)
     {
-        parent::__construct( $properties);
-        
+        parent::__construct($properties);
+
         $this->_scopeType = $properties['scope_type'];
 
         $this->_children = $properties['child'] ?? [];
-        
+
         foreach ($this->_children as $child) {
             parent::addChild($child);
         }
-        
+
         $this->_else = $properties['else'] ?? [];
-        
+
         foreach ($this->_else as $else) {
             parent::addChild($else);
         }
@@ -44,56 +46,51 @@ class RunOnceElement extends AbstractWorkflowContainerComponent implements IConv
 
     public function read(\Convo\Core\Workflow\IConvoRequest $request, \Convo\Core\Workflow\IConvoResponse $response)
     {
-        if ( !$this->_isTriggered()) 
-        {
-            $this->_logger->info('One-off element hasn\'t fired yet in scope ['.$this->_scopeType.']');
-            
-            foreach ( $this->_children as $child)
-            {
+        if (!$this->_isTriggered()) {
+            $this->_logger->info('One-off element hasn\'t fired yet in scope [' . $this->_scopeType . ']');
+
+            foreach ($this->_children as $child) {
                 try {
                     $child->read($request, $response);
-                }
-                catch (StateChangedException $e) {
-                    $this->_logger->info( 'State changed while reading children. Setting triggered to true before transition.');
+                } catch (StateChangedException $e) {
+                    $this->_logger->info('State changed while reading children. Setting triggered to true before transition.');
                     $this->_markAsTriggered();
                     throw $e;
                 }
             }
 
             $this->_markAsTriggered();
-        }
-        else 
-        {
-            $this->_logger->info('One-off element already fired in ['.$this->_scopeType.'] scope. Checking for else elements');
+        } else {
+            $this->_logger->info('One-off element already fired in [' . $this->_scopeType . '] scope. Checking for else elements');
 
             foreach ($this->_else as $else) {
                 $else->read($request, $response);
             }
         }
     }
-    
+
     private function _getParams()
     {
-        $scope_type =   $this->evaluateString( $this->_scopeType);
-        return $this->getService()->getComponentParams( $scope_type, $this);
+        $scope_type =   $this->evaluateString($this->_scopeType);
+        return $this->getService()->getComponentParams($scope_type, $this);
     }
-    
+
     private function _isTriggered()
     {
-        return $this->_getParams()->getServiceParam( self::PARAM_NAME_TRIGGERED);
+        return $this->_getParams()->getServiceParam(self::PARAM_NAME_TRIGGERED);
     }
-    
+
     private function _markAsTriggered()
     {
-        $this->_getParams()->setServiceParam( self::PARAM_NAME_TRIGGERED, true);
+        $this->_getParams()->setServiceParam(self::PARAM_NAME_TRIGGERED, true);
     }
-    
+
     public function isEnabled()
     {
-        if ( !empty( $this->_else)) {
+        if (!empty($this->_else)) {
             return true;
         }
-        
-        return $this->evaluateString( $this->_test);
+
+        return $this->evaluateString($this->_scopeType);
     }
 }
