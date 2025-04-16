a41cdd1e5ae80bc55c4687b0974d55f81b6a0845
Tihomir Dmitrovic <tole.car@gmail.com>
2025-04-16 14:57:22 +0200

    Remove alexa templates

--- src/Convo/Pckg/Core/CorePackageDefinition.php
+++ src/Convo/Pckg/Core/CorePackageDefinition.php
@@ -47,8 +47,6 @@ class CorePackageDefinition extends AbstractPackageDefinition
         parent::__construct($logger, self::NAMESPACE, __DIR__);
 
         $this->registerTemplate(__DIR__ . '/blank.template.json');
-        $this->registerTemplate(__DIR__ . '/basic.template.json');
-        $this->registerTemplate(__DIR__ . '/convo-daily-quotes.template.json');
     }
 
     protected function _initIntents()
